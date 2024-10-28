6f9415f869a23f0cda80600bc693111c41c75a53
Raman Sahu <ramansahu264@gmail.com>
2024-10-28 13:27:36 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+# import random
+# import pandas as pd
+
+# # Simulate bin data
+# data = {
+#     'Bin_ID': [1, 2, 3, 4, 5],
+#     'Location': ['Area 1', 'Area 2', 'Area 3', 'Area 4', 'Area 5'],
+#     'Fill_Level': [random.randint(0, 100) for _ in range(5)],  # Random fill level between 0-100%
+#     'Distance': [random.randint(1, 10) for _ in range(5)]  # Distance in km
+# }
+# df = pd.DataFrame(data)
+# print(df)
